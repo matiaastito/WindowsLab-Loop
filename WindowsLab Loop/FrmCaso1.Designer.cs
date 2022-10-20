6b68f072b0e0beaa1eab25ebc7f708ad133c3b59
Matias <matias.tito27@gmail.com>
2022-10-20 17:54:46 -0300

    Agregar archivos de proyecto.

---
+++ WindowsLab Loop/FrmCaso1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WindowsLab_Loop
+{
+    partial class FrmCaso1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIngresoAlumnos = new System.Windows.Forms.Button();
+            this.lstVector = new System.Windows.Forms.ListBox();
+            this.vectorPromedio = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnIngresoAlumnos
+            // 
+            this.btnIngresoAlumnos.Location = new System.Drawing.Point(107, 111);
+            this.btnIngresoAlumnos.Name = "btnIngresoAlumnos";
+            this.btnIngresoAlumnos.Size = new System.Drawing.Size(155, 54);
+            this.btnIngresoAlumnos.TabIndex = 0;
+            this.btnIngresoAlumnos.Text = "Cargar alumnos + notas";
+            this.btnIngresoAlumnos.UseVisualStyleBackColor = true;
+            this.btnIngresoAlumnos.Click += new System.EventHandler(this.btnIngresoAlumnos_Click);
+            // 
+            // lstVector
+            // 
+            this.lstVector.FormattingEnabled = true;
+            this.lstVector.Location = new System.Drawing.Point(307, 111);
+            this.lstVector.Name = "lstVector";
+            this.lstVector.Size = new System.Drawing.Size(174, 173);
+            this.lstVector.TabIndex = 2;
+            // 
+            // vectorPromedio
+            // 
+            this.vectorPromedio.FormattingEnabled = true;
+            this.vectorPromedio.Location = new System.Drawing.Point(516, 111);
+            this.vectorPromedio.Name = "vectorPromedio";
+            this.vectorPromedio.Size = new System.Drawing.Size(174, 173);
+            this.vectorPromedio.TabIndex = 3;
+            // 
+            // FrmCaso1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.vectorPromedio);
+            this.Controls.Add(this.lstVector);
+            this.Controls.Add(this.btnIngresoAlumnos);
+            this.Name = "FrmCaso1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIngresoAlumnos;
+        private System.Windows.Forms.ListBox lstVector;
+        private System.Windows.Forms.ListBox vectorPromedio;
+    }
+}
+
---
+++ WindowsLab Loop/FrmCaso1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+
+namespace WindowsLab_Loop
+{
+    public partial class FrmCaso1 : Form
+    {
+        private int[] notas = new int[3];
+        private string[] nomAlumno = new string[4];
+        
+        public FrmCaso1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresoAlumnos_Click(object sender, EventArgs e)
+        {
+            cargaAlumnos();
+            cargaNotas(nomAlumno);
+        }
+
+        private void cargaAlumnos()
+        {
+            for (int i = 0; i < nomAlumno.Length; i++)
+            {
+                nomAlumno[i] = Interaction.InputBox("Ingrese el nombre del" + (i+1) + "° alumno");
+            }
+        }
+
+        private void cargaNotas(string[] alumnos)
+        {
+            foreach (string alumnario in alumnos)
+            {
+                for (int i = 0; i < notas.Length; i++)
+                {
+                    notas[i] = int.Parse(Interaction.InputBox("Ingresa la " + (i + 1) + "° nota del alumno " + alumnario)); 
+                }
+
+                mostrarMayorNota(alumnario, notas);
+                mostrarPromedio(alumnario, notas);
+                
+            }
+        }
+
+        private void mostrarPromedio(string alumno, int[] notas)
+        {
+            vectorPromedio.Items.Clear();
+            vectorPromedio.Items.Add("Promedio");
+            vectorPromedio.Items.Add(alumno);
+            double promedio = 0;
+            int suma = 0;
+            int cont = 0;
+            foreach (int nota in notas)
+            {
+                cont++;
+                suma += nota;
+            }
+            promedio = (double) suma / (double) cont;
+            vectorPromedio.Items.Add(promedio);
+            informarCondicion(promedio);
+
+
+        }
+
+        private void informarCondicion(double promedio)
+        {
+            if (promedio < 4)
+            {
+                MessageBox.Show("Debe ir a recuperatorio");
+            }
+            else if(promedio <= 7)
+            {
+                MessageBox.Show("Bien");
+            }
+            else
+            {
+                MessageBox.Show("Muy bien");
+            }
+        }
+        
+        private void mostrarMayorNota(string alumno, int[] notas)
+        {
+            lstVector.Items.Clear();
+            lstVector.Items.Add("Mayor nota");
+            lstVector.Items.Add(alumno);
+            int mayorNota = notas[0];
+            foreach (int nota in notas)
+            {
+                if(nota > mayorNota)
+                mayorNota = nota;
+            }
+            lstVector.Items.Add(mayorNota);
+        }
+
+    }
+}
