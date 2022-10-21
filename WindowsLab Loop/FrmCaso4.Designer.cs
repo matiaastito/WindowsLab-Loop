e8d6d599d9bf3adae123928da7621cb41a583457
Matias <matias.tito27@gmail.com>
2022-10-20 21:33:58 -0300

    Caso 4 completo

---
+++ WindowsLab Loop/FrmCaso4.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsLab_Loop
+{
+    partial class FrmCaso4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCargaDatos = new System.Windows.Forms.Button();
+            this.btnMostarMayores = new System.Windows.Forms.Button();
+            this.lstMayoresEdad = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCargaDatos
+            // 
+            this.btnCargaDatos.Location = new System.Drawing.Point(34, 166);
+            this.btnCargaDatos.Name = "btnCargaDatos";
+            this.btnCargaDatos.Size = new System.Drawing.Size(183, 46);
+            this.btnCargaDatos.TabIndex = 0;
+            this.btnCargaDatos.Text = "Ingresar personas y edad";
+            this.btnCargaDatos.UseVisualStyleBackColor = true;
+            this.btnCargaDatos.Click += new System.EventHandler(this.btnCargaDatos_Click);
+            // 
+            // btnMostarMayores
+            // 
+            this.btnMostarMayores.Location = new System.Drawing.Point(270, 34);
+            this.btnMostarMayores.Name = "btnMostarMayores";
+            this.btnMostarMayores.Size = new System.Drawing.Size(145, 45);
+            this.btnMostarMayores.TabIndex = 1;
+            this.btnMostarMayores.Text = "Mostrar Mayores de Edad (+18)";
+            this.btnMostarMayores.UseVisualStyleBackColor = true;
+            this.btnMostarMayores.Click += new System.EventHandler(this.btnMostarMayores_Click);
+            // 
+            // lstMayoresEdad
+            // 
+            this.lstMayoresEdad.FormattingEnabled = true;
+            this.lstMayoresEdad.Location = new System.Drawing.Point(270, 106);
+            this.lstMayoresEdad.Name = "lstMayoresEdad";
+            this.lstMayoresEdad.Size = new System.Drawing.Size(145, 160);
+            this.lstMayoresEdad.TabIndex = 2;
+            // 
+            // FrmCaso4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstMayoresEdad);
+            this.Controls.Add(this.btnMostarMayores);
+            this.Controls.Add(this.btnCargaDatos);
+            this.Name = "FrmCaso4";
+            this.Text = "FrmCaso4";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCargaDatos;
+        private System.Windows.Forms.Button btnMostarMayores;
+        private System.Windows.Forms.ListBox lstMayoresEdad;
+    }
+}
---
+++ WindowsLab Loop/FrmCaso4.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace WindowsLab_Loop
+{
+    public partial class FrmCaso4 : Form
+    {
+        string[] personasNombre = new string[5];
+        int[] personasEdad = new int[5];
+
+        public FrmCaso4()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCargaDatos_Click(object sender, EventArgs e)
+        {
+            cargaPersonas();
+        }
+
+        private void btnMostarMayores_Click(object sender, EventArgs e)
+        {
+            mostrarMayoresEdad();
+        }
+
+        #region ABM
+        private void cargaPersonas()
+        {
+            for (int i = 0; i < personasNombre.Length; i++)
+            {
+                personasNombre[i] = Interaction.InputBox("Ingrese nombre y apellido", "Carga de personas");
+                personasEdad[i] = int.Parse(Interaction.InputBox("Ingrese la edad", "Carga edad"));
+            }
+        }
+
+        #endregion
+
+        #region muestra
+
+        private void mostrarMayoresEdad()
+        {
+            int cont = 0;
+            foreach (var persona in personasNombre)
+            {
+                if (personasEdad[cont] >= 18)
+                    lstMayoresEdad.Items.Add(persona);
+                cont++;
+            }
+        }
+
+        #endregion
+    }
+}
