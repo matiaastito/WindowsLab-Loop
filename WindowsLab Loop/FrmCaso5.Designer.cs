54fce893c0ae400fd56945d3fc33d38db211257d
Matias <matias.tito27@gmail.com>
2022-10-21 00:46:05 -0300

    Agrego caso 5

---
+++ WindowsLab Loop/FrmCaso5.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WindowsLab_Loop
+{
+    partial class FrmCaso5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIngresarProvincias = new System.Windows.Forms.Button();
+            this.btnTemperaturasMedias = new System.Windows.Forms.Button();
+            this.btnMostrarMesesxTemp = new System.Windows.Forms.Button();
+            this.lstMesesxTemp = new System.Windows.Forms.ListBox();
+            this.btnCalcularMediaTrimestral = new System.Windows.Forms.Button();
+            this.btnMuestraMediasTrim = new System.Windows.Forms.Button();
+            this.lstMediasTrimestrales = new System.Windows.Forms.ListBox();
+            this.btnMostrarMayorProvinciaTemp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnIngresarProvincias
+            // 
+            this.btnIngresarProvincias.Location = new System.Drawing.Point(58, 48);
+            this.btnIngresarProvincias.Name = "btnIngresarProvincias";
+            this.btnIngresarProvincias.Size = new System.Drawing.Size(153, 45);
+            this.btnIngresarProvincias.TabIndex = 0;
+            this.btnIngresarProvincias.Text = "Ingresar Provincias";
+            this.btnIngresarProvincias.UseVisualStyleBackColor = true;
+            this.btnIngresarProvincias.Click += new System.EventHandler(this.btnIngresarProvincias_Click);
+            // 
+            // btnTemperaturasMedias
+            // 
+            this.btnTemperaturasMedias.Location = new System.Drawing.Point(58, 123);
+            this.btnTemperaturasMedias.Name = "btnTemperaturasMedias";
+            this.btnTemperaturasMedias.Size = new System.Drawing.Size(153, 54);
+            this.btnTemperaturasMedias.TabIndex = 1;
+            this.btnTemperaturasMedias.Text = "Ingresar temperaturas mensuales p/ provincia";
+            this.btnTemperaturasMedias.UseVisualStyleBackColor = true;
+            this.btnTemperaturasMedias.Click += new System.EventHandler(this.btnTemperaturasMedias_Click);
+            // 
+            // btnMostrarMesesxTemp
+            // 
+            this.btnMostrarMesesxTemp.Location = new System.Drawing.Point(260, 48);
+            this.btnMostrarMesesxTemp.Name = "btnMostrarMesesxTemp";
+            this.btnMostrarMesesxTemp.Size = new System.Drawing.Size(226, 45);
+            this.btnMostrarMesesxTemp.TabIndex = 2;
+            this.btnMostrarMesesxTemp.Text = "Mostrar provincias con sus medias mensuales";
+            this.btnMostrarMesesxTemp.UseVisualStyleBackColor = true;
+            this.btnMostrarMesesxTemp.Click += new System.EventHandler(this.btnMostrarMesesxTemp_Click);
+            // 
+            // lstMesesxTemp
+            // 
+            this.lstMesesxTemp.FormattingEnabled = true;
+            this.lstMesesxTemp.Location = new System.Drawing.Point(260, 123);
+            this.lstMesesxTemp.Name = "lstMesesxTemp";
+            this.lstMesesxTemp.Size = new System.Drawing.Size(226, 290);
+            this.lstMesesxTemp.TabIndex = 3;
+            // 
+            // btnCalcularMediaTrimestral
+            // 
+            this.btnCalcularMediaTrimestral.Location = new System.Drawing.Point(58, 205);
+            this.btnCalcularMediaTrimestral.Name = "btnCalcularMediaTrimestral";
+            this.btnCalcularMediaTrimestral.Size = new System.Drawing.Size(153, 54);
+            this.btnCalcularMediaTrimestral.TabIndex = 4;
+            this.btnCalcularMediaTrimestral.Text = "Calcular media Trimestral";
+            this.btnCalcularMediaTrimestral.UseVisualStyleBackColor = true;
+            this.btnCalcularMediaTrimestral.Click += new System.EventHandler(this.btnCalcularMediaTrimestral_Click);
+            // 
+            // btnMuestraMediasTrim
+            // 
+            this.btnMuestraMediasTrim.Location = new System.Drawing.Point(524, 48);
+            this.btnMuestraMediasTrim.Name = "btnMuestraMediasTrim";
+            this.btnMuestraMediasTrim.Size = new System.Drawing.Size(185, 45);
+            this.btnMuestraMediasTrim.TabIndex = 5;
+            this.btnMuestraMediasTrim.Text = "Mostrar Medias Trimestrales";
+            this.btnMuestraMediasTrim.UseVisualStyleBackColor = true;
+            this.btnMuestraMediasTrim.Click += new System.EventHandler(this.btnMuestraMediasTrim_Click);
+            // 
+            // lstMediasTrimestrales
+            // 
+            this.lstMediasTrimestrales.FormattingEnabled = true;
+            this.lstMediasTrimestrales.Location = new System.Drawing.Point(524, 123);
+            this.lstMediasTrimestrales.Name = "lstMediasTrimestrales";
+            this.lstMediasTrimestrales.Size = new System.Drawing.Size(185, 290);
+            this.lstMediasTrimestrales.TabIndex = 6;
+            // 
+            // btnMostrarMayorProvinciaTemp
+            // 
+            this.btnMostrarMayorProvinciaTemp.Location = new System.Drawing.Point(58, 297);
+            this.btnMostrarMayorProvinciaTemp.Name = "btnMostrarMayorProvinciaTemp";
+            this.btnMostrarMayorProvinciaTemp.Size = new System.Drawing.Size(153, 60);
+            this.btnMostrarMayorProvinciaTemp.TabIndex = 7;
+            this.btnMostrarMayorProvinciaTemp.Text = "Mostrar provincia con mayor temperatura promedio trimestral";
+            this.btnMostrarMayorProvinciaTemp.UseVisualStyleBackColor = true;
+            this.btnMostrarMayorProvinciaTemp.Click += new System.EventHandler(this.btnMostrarMayorProvinciaTemp_Click);
+            // 
+            // FrmCaso5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMostrarMayorProvinciaTemp);
+            this.Controls.Add(this.lstMediasTrimestrales);
+            this.Controls.Add(this.btnMuestraMediasTrim);
+            this.Controls.Add(this.btnCalcularMediaTrimestral);
+            this.Controls.Add(this.lstMesesxTemp);
+            this.Controls.Add(this.btnMostrarMesesxTemp);
+            this.Controls.Add(this.btnTemperaturasMedias);
+            this.Controls.Add(this.btnIngresarProvincias);
+            this.Name = "FrmCaso5";
+            this.Text = "FrmCaso5";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIngresarProvincias;
+        private System.Windows.Forms.Button btnTemperaturasMedias;
+        private System.Windows.Forms.Button btnMostrarMesesxTemp;
+        private System.Windows.Forms.ListBox lstMesesxTemp;
+        private System.Windows.Forms.Button btnCalcularMediaTrimestral;
+        private System.Windows.Forms.Button btnMuestraMediasTrim;
+        private System.Windows.Forms.ListBox lstMediasTrimestrales;
+        private System.Windows.Forms.Button btnMostrarMayorProvinciaTemp;
+    }
+}
---
+++ WindowsLab Loop/FrmCaso5.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace WindowsLab_Loop
+{
+    public partial class FrmCaso5 : Form
+    {
+        string[] provincias = new string[4];
+        double[] temperaturaMensual = new double[12];
+        double[] mediasTrimestrales = new double[4]; 
+
+        public FrmCaso5()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresarProvincias_Click(object sender, EventArgs e)
+        {
+            cargarProvincias();
+        }
+
+        private void btnTemperaturasMedias_Click(object sender, EventArgs e)
+        {
+            cargarTemperaturasMedias();  
+        }
+
+        private void btnCalcularMediaTrimestral_Click(object sender, EventArgs e)
+        {
+            calcularMediasTrimestrales();
+        }
+
+        private void btnMostrarMayorProvinciaTemp_Click(object sender, EventArgs e)
+        {
+            mostrarProvinciaMayorMedia();
+        }
+
+        private void btnMostrarMesesxTemp_Click(object sender, EventArgs e)
+        {
+            mostrarProvinciasConTemperaturas();
+        }
+
+        private void btnMuestraMediasTrim_Click(object sender, EventArgs e)
+        {
+            mostrarProvinciasConMedias();
+        }
+
+        #region ABM
+
+        private void cargarProvincias()
+        {
+            for (int i = 0; i < provincias.Length; i++)
+            {
+                provincias[i] = Interaction.InputBox("Ingrese el nombre de la Provincia");
+            }
+        }
+
+        private void cargarTemperaturasMedias()
+        {
+            for (int i = 0; i < temperaturaMensual.Length; i++)
+            {
+                if (i<3)
+                {
+                    temperaturaMensual[i] = double.Parse(Interaction.InputBox("Ingresar temperatura mensual", provincias[0]));
+                }
+                else if (i < 6)
+                {
+                    temperaturaMensual[i] = double.Parse(Interaction.InputBox("Ingresar temperatura mensual", provincias[1]));
+                }
+                else if (i < 9)
+                {
+                    temperaturaMensual[i] = double.Parse(Interaction.InputBox("Ingresar temperatura mensual", provincias[2]));
+                }
+                else
+                {
+                    temperaturaMensual[i] = double.Parse(Interaction.InputBox("Ingresar temperatura mensual", provincias[3]));
+                }
+
+            }
+        }
+
+        #endregion
+
+        #region calculos
+
+        private void calcularMediasTrimestrales()
+        {
+            for (int i = 0; i < temperaturaMensual.Length; i++)
+            {
+                if (i < 3)
+                {
+                    mediasTrimestrales[0] = mediasTrimestrales[0] + temperaturaMensual[i];
+                }
+                else if (i < 6)
+                {
+                    mediasTrimestrales[1] = mediasTrimestrales[1] + temperaturaMensual[i];
+                }
+                else if (i < 9)
+                {
+                    mediasTrimestrales[2] = mediasTrimestrales[2] + temperaturaMensual[i];
+                }
+                else
+                {
+                    mediasTrimestrales[3] = mediasTrimestrales[3] + temperaturaMensual[i];
+                }
+            }
+            for (int i = 0; i < mediasTrimestrales.Length; i++)
+            {
+                mediasTrimestrales[i] /= 3;
+            }
+        }
+
+        private int encontrarMayorMedia()
+        {
+            double mayorMedia = 0;
+            int resultado = 0;
+            int cont = 0;
+
+            foreach (var mediaTrim in mediasTrimestrales)
+            {
+                if (mediaTrim > mayorMedia)
+                {
+                    mayorMedia = mediaTrim;
+                    resultado = cont;
+                }
+                cont++;
+            }
+            return resultado;
+        }
+
+
+        #endregion
+
+        #region metodos mostrar
+
+        private void mostrarProvinciasConMedias()
+        {
+            int cont = 0;
+            foreach (var provincia in provincias)
+            {
+                lstMediasTrimestrales.Items.Add(provincia);
+                lstMediasTrimestrales.Items.Add(mediasTrimestrales[cont]);
+                lstMediasTrimestrales.Items.Add("\n");
+                cont++;
+            }
+        }
+
+        private void mostrarProvinciasConTemperaturas()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if(i == 0)
+                {
+                    lstMesesxTemp.Items.Add(provincias[i]);
+                    for (int cont = 0; cont < 3; cont++)
+                    {
+                        lstMesesxTemp.Items.Add(temperaturaMensual[cont]);
+                    }
+                }
+                else if (i == 1)
+                {
+                    lstMesesxTemp.Items.Add("\n");
+                    lstMesesxTemp.Items.Add(provincias[i]);
+                    for (int cont = 3; cont < 6; cont++)
+                    {
+                        lstMesesxTemp.Items.Add(temperaturaMensual[cont]);
+                    }
+                }
+                else if (i == 2)
+                {
+                    lstMesesxTemp.Items.Add("\n");
+                    lstMesesxTemp.Items.Add(provincias[i]);
+                    for (int cont = 6; cont < 9; cont++)
+                    {
+                        lstMesesxTemp.Items.Add(temperaturaMensual[cont]);
+                    }
+                }
+                else
+                {
+                    lstMesesxTemp.Items.Add("\n");
+                    lstMesesxTemp.Items.Add(provincias[i]);
+                    for (int cont = 9; cont < 12; cont++)
+                    {
+                        lstMesesxTemp.Items.Add(temperaturaMensual[cont]);
+                    }
+                }
+            }
+        }
+
+        private void mostrarProvinciaMayorMedia()
+        {
+            int resultado = encontrarMayorMedia();
+            MessageBox.Show("La provincia con mayor promedio de temperatura trimestral es " + provincias[resultado] + " y la temperatura es " + mediasTrimestrales[resultado]);
+        }
+
+        #endregion
+
+
+    }
+}
